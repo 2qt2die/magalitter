4494ae538650516cf7991e927e389895c020c676
2qt2die <181026268+2qt2die@users.noreply.github.com>
2024-10-11 15:45:29 -0300

    fix twitter support

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from atproto_client import exceptions as exceptions_at
 from json import JSONDecodeError
 from dotenv import load_dotenv
 from atproto import Client, models
-from tweepy import OAuthHandler, API
+from tweepy import Client as x_Client
 from tweepy.errors import TweepyException
 import warnings
 from helpers import HTMLCleaner, create_hashtag_facet, fetch_and_create_ogp_embed
@@ -39,6 +39,7 @@ class MagalitterBot:
         self.time_interval_seconds = self.time_interval_hours * 3600
         self.fallback_image = os.getenv('FALLBACK_IMAGE').format(domain=self.domain_name)
         self.tweeted_post_file = 'tweeted_posts.txt'
+        self.bluesky_post_file = 'bluesky_posts.txt'
 
         self.twitter_api = self.init_twitter() if self.enable_twitter else None
         self.bluesky_client = self.init_bluesky() if self.enable_bluesky else None
@@ -46,11 +47,16 @@ class MagalitterBot:
     def init_twitter(self):
         """Initialize Twitter API using Tweepy."""
         try:
-            auth = OAuthHandler(os.getenv('API_KEY'), os.getenv('API_SECRET_KEY'))
-            auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
-            api = API(auth)
+            client = x_Client(
+                bearer_token=os.getenv('BEARER_TOKEN'),
+                consumer_key=os.getenv('API_KEY'),
+                consumer_secret=os.getenv('API_SECRET_KEY'),
+                access_token=os.getenv('ACCESS_TOKEN'),
+                access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'),
+                wait_on_rate_limit=True
+            )
             logging.info("Twitter API initialized successfully")
-            return api
+            return client
         except TweepyException as e:
             logging.error(f"Error initializing Twitter API: {e}")
             raise
@@ -69,16 +75,16 @@ class MagalitterBot:
             logging.error(f"Error initializing Bluesky client: {e}")
             raise
 
-    def get_tweeted_post_ids(self) -> set:
+    def get_posted_ids(self, plataform_file: str) -> set:
         """Read already tweeted post IDs from file."""
-        if os.path.exists(self.tweeted_post_file):
-            with open(self.tweeted_post_file, 'r') as file:
+        if os.path.exists(plataform_file):
+            with open(plataform_file, 'r') as file:
                 return set(file.read().splitlines())
         return set()
 
-    def save_tweeted_post_id(self, post_id: int, board_dir: str):
+    def save_posted_id(self, post_id: int, board_dir: str, plataform_file: str):
         """Save the post ID to avoid future duplication."""
-        with open(self.tweeted_post_file, 'a') as file:
+        with open(plataform_file, 'a') as file:
             file.write(f"{board_dir}:{post_id}\n")
 
     def fetch_posts(self) -> t.List[dict]:
@@ -103,12 +109,11 @@ class MagalitterBot:
         """Format the message using the template from the .env file."""
         board = post.get('board')
         sub = post.get('sub', '').strip()
-        com = self.strip_html(post.get('com')).strip()[:150]  # Limit to 150 chars
-        url = f"{self.domain_name}/{board}/res/{post.get('no')}"
+        com = self.strip_html(post.get('com')).strip()[:130]  # Limit to 130 chars
 
         if sub:
-            return self.post_format.format(board=board, sub=f"{sub} -", com=com, url=url)
-        return self.post_format.replace("{sub}", "").format(board=board, com=com, url=url)
+            return self.post_format.format(board=board, sub=f"{sub} -", com=com)
+        return self.post_format.replace("{sub}", "").format(board=board, com=com)
 
     def post_to_bluesky(self, message: str, url: t.Optional[str] = None):
         """Post the message to Bluesky, optionally with media or external resource."""
@@ -118,16 +123,21 @@ class MagalitterBot:
 
         embed = None
         hashtag = f"#{self.hashtag_name}"
-        message += f"{hashtag}"
 
         facets = create_hashtag_facet(message, self.hashtag_name)
 
         if url:
             embed = fetch_and_create_ogp_embed(url, self.bluesky_client, self.fallback_image)
 
+        max_message_length = 300 - len(hashtag)
+        if len(message) > max_message_length:
+            message = message[:max_message_length - 3] + '...'
+
+        bluesky_content = f"{message}\n\n{hashtag}"
+
         try:
-            self.bluesky_client.send_post(text=message, facets=facets, embed=embed)
-            logging.info(f"Posted on Bluesky: {message}")
+            self.bluesky_client.send_post(text=bluesky_content, facets=facets, embed=embed)
+            logging.info(f"Posted on Bluesky: {bluesky_content}")
             return True
         except Exception as e:
             if isinstance(e.__cause__, ConnectionRefusedError) and e.__cause__.errno == 111:
@@ -137,17 +147,25 @@ class MagalitterBot:
                 logging.error(f"Failed to post on Bluesky: {e}")
                 return False 
 
-    def post_to_twitter(self, message: str):
+    def post_to_twitter(self, message: str, url: str):
         """Post the message to Twitter."""
         if not self.enable_twitter or not self.twitter_api:
             logging.info("Twitter posting is disabled.")
             return True
 
-        message += f"#{self.hashtag_name}"
+        suffix = os.getenv('TWITTER_SUFFIX', 'See more at: {url}\n\n').format(url=url)
+        hashtag = f"#{self.hashtag_name}"
+
+        max_message_length = 280 - len(suffix) - len(hashtag) 
+        if len(message) > max_message_length:
+            message = message[:max_message_length - 3] + '...'
+
+        tweet_content = f"{message}{suffix}{hashtag}"
 
         try:
-            self.twitter_api.update_status(message)
-            logging.info(f"Tweeted: {message}")
+            response = self.twitter_api.create_tweet(text=tweet_content)
+            logging.info(f"Tweeted: {tweet_content}")
+            logging.info(f"Twitter Response: {response}")
             return True
         except TweepyException as e:
             if isinstance(e.__cause__, ConnectionRefusedError) and e.__cause__.errno == 111:
@@ -157,41 +175,76 @@ class MagalitterBot:
                 logging.error(f"Failed to tweet: {e}")
                 return False
 
+    def should_skip_post(self, post_flags: dict, current_time: float) -> bool:
+        """Check whether the post should be skipped due to age, stickiness, or lock."""
+
+        if current_time - post_flags['time'] < self.time_interval_seconds:
+            logging.info(f"Skipping thread #{post_flags['no']} from /{post_flags['board']}/ - Too recent (posted {time_difference / 3600:.2f} hours ago).")
+            return True
+
+        if post_flags['sticky'] == 1 or post_flags['locked'] == 1:
+            logging.info(f"Skipping thread #{post_flags['no']} from /{post_flags['board']}/. Sticky: {post_flags['sticky']}, Locked: {post_flags['locked']}.")
+            return True
+
+        return False
+
+    def post_to_platforms(self, post_id: int, board_dir: str, save_id: str, tweeted_post_ids: set, bluesky_post_ids: set, message: str, url: str):
+        """Post to Twitter and Bluesky, and track the post IDs."""
+
+        if save_id not in tweeted_post_ids:
+            logging.info(f"Attempting to post to Twitter: Thread #{post_id} from board /{board_dir}/")
+            if self.post_to_twitter(message, url=url):
+                self.save_posted_id(post_id, board_dir, self.tweeted_post_file)
+            else:
+                logging.info(f"Not saving Thread #{post_id} due to error on Twitter.")
+        else:
+            logging.info(f"Thread #{post_id} already tweeted. Skipping Twitter.")
+
+        if save_id not in bluesky_post_ids:
+            logging.info(f"Attempting to post to Bluesky: Thread #{post_id} from board /{board_dir}/")
+            if self.post_to_bluesky(message, url=url):
+                self.save_posted_id(post_id, board_dir, self.bluesky_post_file)
+            else:
+                logging.info(f"Not saving Thread #{post_id} due to error on Bluesky.")
+        else:
+            logging.info(f"Thread #{post_id} already posted to Bluesky. Skipping Bluesky.")
 
     def run(self):
         """Main bot logic: fetch posts, tweet and post to Bluesky."""
-        tweeted_post_ids = self.get_tweeted_post_ids()
+        tweeted_post_ids = self.get_posted_ids(self.tweeted_post_file)
+        bluesky_post_ids = self.get_posted_ids(self.bluesky_post_file)
         first_posts = self.fetch_posts()
 
         current_time = time.time()
 
+        if not first_posts:
+            logging.error("No posts fetched. Bye...")
+            return
+
+        logging.info(f"Posts fetched: {len(first_posts)} posts")
+
         for post in first_posts:
             post_id = post.get('no')
+            post_board = post.get('board')
             post_time = post.get('time')
-            board_dir = post.get('board')
 
-            if current_time - post_time < self.time_interval_seconds:
-                logging.info(f"Thread #{post_id} is less than {self.time_interval_hours} hours old. Skipping.")
-                continue
+            post_flags = {
+                'time': post_time,
+                'no': post_id,
+                'board': post_board,
+                'locked': post.get('locked', 0),
+                'sticky': post.get('sticky', 0)
+            }
 
-            if post.get('sticky') == 1 or post.get('locked') == 1:
-                logging.info(f"Skipping thread #{post_id}. Sticky: {post.get('sticky')}, Locked: {post.get('locked')}.")
-                continue
+            save_id = f"{post_board}:{post_id}"
 
-            if str(f"{board_dir}:{post_id}") in tweeted_post_ids:
-                logging.info(f"Thread #{post_id} from {board_dir} already tweeted. Skipping.")
+            if self.should_skip_post(post_flags, current_time):
                 continue
 
             message = self.format_message(post)
-            url = f"{self.domain_name}/{board_dir}/res/{post_id}"
+            url = f"{self.domain_name}/{post_board}/res/{post_id}"
 
-            twitter_ret = self.post_to_twitter(message)
-            bsky_ret = self.post_to_bluesky(message, url=url)
-            if not twitter_ret or not bsky_ret:
-                logging.info(f"Not saving post ID {post_id} due to error.")
-                continue
-
-            self.save_tweeted_post_id(post_id, board_dir)
+            self.post_to_platforms(post_id, post_board, save_id, tweeted_post_ids, bluesky_post_ids, message, url)
 
 if __name__ == "__main__":
     bot = MagalitterBot()
